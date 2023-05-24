3e4992a98fd754993696f26a32a834783479a657
owbird <owbirdphorccuo@gmail.com>
2023-05-24 10:42:11 +0000

    Feat: Build script added

---
+++ build.sh
@@ -0,0 +1,36 @@
+clear
+
+rm -rf build
+
+
+wails build -o onedev 
+
+mkdir -p build/bin/DEBIAN
+mkdir -p build/bin/usr/bin
+
+mv build/bin/onedev build/bin/usr/bin
+
+touch build/bin/DEBIAN/control
+
+echo "Package: onedev" >> build/bin/DEBIAN/control
+echo "Version: 1" >> build/bin/DEBIAN/control
+echo "Architecture: amd64" >> build/bin/DEBIAN/control
+echo "Maintainer: OneDev" >> build/bin/DEBIAN/control
+echo "Description: All in one dev tools" >> build/bin/DEBIAN/control
+
+
+chmod +x build/bin/usr/bin
+
+mkdir -p build/bin/usr/share/icons/
+
+cp appicon.png build/bin/usr/share/icons/appicon.png
+
+chmod 644 build/bin/usr/share/icons/appicon.png
+
+
+dpkg-deb --build build/bin  onedev.deb
+
+
+
+
+
