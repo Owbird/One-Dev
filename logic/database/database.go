c25fa4c89b09bfc339b0dfed3eb4ecc0bffd3ce5
owbird <owbirdphorccuo@gmail.com>
2023-05-26 20:39:59 +0000

    Chore: Use sync.Mutex

--- logic/database/database.go
+++ logic/database/database.go
@@ -5,12 +5,14 @@ import (
 	"sort"
 	"strings"
 
+	"sync"
+
 	c "github.com/ostafen/clover"
 	"github.com/owbird/one-dev/logic/data"
 )
 
 type Database struct {
-	OpenState chan bool
+	OpenState sync.Mutex
 	Db        *c.DB
 }
 
@@ -18,7 +20,7 @@ var database Database
 
 func (d *Database) Close() {
 	d.Db.Close()
-	d.ToggleState()
+	d.OpenState.Lock()
 }
 
 func (d *Database) Open() {
@@ -29,32 +31,6 @@ func (d *Database) Open() {
 	}
 
 	d.Db = db
-
-	d.ToggleState()
-
-}
-
-func (d *Database) ToggleState() {
-
-	if d.OpenState == nil {
-		d.OpenState = make(chan bool, 1)
-		d.OpenState <- true
-
-		return
-	}
-
-	d.OpenState <- !<-d.OpenState
-
-}
-
-func (d *Database) IsOpen() bool {
-
-	current_state := <-d.OpenState
-
-	// Replace state buffer so its not empty!
-	d.OpenState <- current_state
-
-	return current_state
 
 }
 
@@ -67,38 +43,33 @@ func Init() {
 }
 
 func IndexGitDir(dir data.File) {
+	database.OpenState.Lock()
 
-	if database.IsOpen() {
+	defer database.OpenState.Unlock()
 
-		database.ToggleState()
+	err := database.Db.CreateCollection("git_dirs")
 
-		defer database.ToggleState()
+	if err != c.ErrCollectionExist {
+		log.Println(err)
+	}
 
-		err := database.Db.CreateCollection("git_dirs")
+	exists, err := database.Db.Query("git_dirs").Where((*c.Criteria)(c.Field("dir").Eq(dir.Dir))).FindFirst()
 
-		if err != c.ErrCollectionExist {
-			log.Println(err)
-		}
+	if err != nil {
+		log.Println(err)
+	}
 
-		exists, err := database.Db.Query("git_dirs").Where((*c.Criteria)(c.Field("dir").Eq(dir.Dir))).FindFirst()
+	if exists == nil {
+		doc := c.NewDocument()
+
+		doc.Set("dir", dir.Dir)
+		doc.Set("parentDir", dir.ParentDir)
+
+		_, err = database.Db.InsertOne("git_dirs", doc)
 
 		if err != nil {
 			log.Println(err)
 		}
-
-		if exists == nil {
-			doc := c.NewDocument()
-
-			doc.Set("dir", dir.Dir)
-			doc.Set("parentDir", dir.ParentDir)
-
-			_, err = database.Db.InsertOne("git_dirs", doc)
-
-			if err != nil {
-				log.Println(err)
-			}
-		}
-
 	}
 
 }
@@ -107,33 +78,29 @@ func GetGitDirs() []data.File {
 
 	dirs := []data.File{}
 
-	if database.IsOpen() {
+	database.OpenState.Lock()
 
-		database.ToggleState()
+	defer database.OpenState.Unlock()
 
-		defer database.ToggleState()
+	log.Println("[+] Getting Git dirs")
 
-		log.Println("[+] Getting Git dirs")
+	docs, err := database.Db.Query("git_dirs").FindAll()
 
-		docs, err := database.Db.Query("git_dirs").FindAll()
-
-		if err != nil {
-			log.Println(err)
-		}
-
-		for _, doc := range docs {
-			dir := &data.File{}
-			doc.Unmarshal(dir)
-
-			dirs = append(dirs, *dir)
-		}
-
-		sort.Slice(dirs, func(i, j int) bool {
-
-			return strings.Compare(dirs[i].Dir, dirs[j].Dir) < 0
-		})
-
+	if err != nil {
+		log.Println(err)
 	}
+
+	for _, doc := range docs {
+		dir := &data.File{}
+		doc.Unmarshal(dir)
+
+		dirs = append(dirs, *dir)
+	}
+
+	sort.Slice(dirs, func(i, j int) bool {
+
+		return strings.Compare(dirs[i].Dir, dirs[j].Dir) < 0
+	})
 
 	return dirs
 }
