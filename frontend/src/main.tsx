1beed24e54ab3149bb27a287b15d6ebbb5438689
owbird <owbirdphorccuo@gmail.com>
2023-06-25 19:37:11 +0000

    Fix: Reactdom render fix

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,12 @@
 import { ChakraProvider } from "@chakra-ui/react";
-import { render } from "react-dom";
+import { createRoot } from "react-dom/client";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import App from "./App";
 import RepoView from "./pages/git/RepoView";
-
 const container = document.getElementById("root");
 
-render(
+const root = createRoot(document.getElementById("root") as HTMLElement);
+root.render(
   <ChakraProvider>
     <Router>
       <Routes>
@@ -14,6 +14,5 @@ render(
         <Route path="/repo/view" element={<RepoView />} />
       </Routes>
     </Router>
-  </ChakraProvider>,
-  container
+  </ChakraProvider>
 );
