48d7966b6f3d5b6c99a8404ea2689b9ef9858f06
owbird <owbirdphorccuo@gmail.com>
2023-08-10 15:10:11 +0000

    Fix: Greeting fix

--- frontend/src/components/home/Greeting.tsx
+++ frontend/src/components/home/Greeting.tsx
@@ -4,65 +4,67 @@ import { Fragment, useEffect, useState } from "react";
 import { FaMoon, FaSun } from "react-icons/fa";
 
 const Greeting = ({ userMeta }: { userMeta: data.UserMeta }) => {
-    const [time, setTime] = useState(new Date());
-    const [greeting, setGreeting] = useState("");
-    const [icon, setIcon] = useState(<></>);
+  const [time, setTime] = useState(new Date());
+  const [greeting, setGreeting] = useState("");
+  const [icon, setIcon] = useState(<></>);
 
-    const timeOptions: Intl.DateTimeFormatOptions = {
-        weekday: "long",
-        year: "numeric",
-        month: "long",
-        day: "numeric",
-        hour: "numeric",
-        minute: "numeric",
-        second: "numeric",
-        hour12: true,
-    };
+  const timeOptions: Intl.DateTimeFormatOptions = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+    hour12: true,
+  };
 
-    useEffect(() => {
-        const timerID = setInterval(() => {
-            const now = new Date();
+  const greet = () => {
+    const now = new Date();
 
-            const hour = now.getHours();
+    const hour = now.getHours();
 
-            if (hour >= 0 && hour < 12) {
-                setGreeting("Good Morning");
-                setIcon(<FaSun color="orange" size={50} />);
-            } else if (hour >= 12 && hour < 16) {
-                setGreeting("Good Afternoon");
-                setIcon(<FaSun color="yellow" size={50} />);
-            } else if (hour >= 16 && hour <= 23) {
-                setIcon(<FaMoon color="orange.200" size={50} />);
-                setGreeting("Good Evening");
-            }
+    if (hour >= 0 && hour < 12) {
+      setGreeting("Good Morning");
+      setIcon(<FaSun color="orange" size={50} />);
+    } else if (hour >= 12 && hour < 16) {
+      setGreeting("Good Afternoon");
+      setIcon(<FaSun color="yellow" size={50} />);
+    } else if (hour >= 16 && hour <= 23) {
+      setIcon(<FaMoon color="orange.200" size={50} />);
+      setGreeting("Good Evening");
+    }
 
-            setTime(now);
-        }, 1000);
-        return () => clearInterval(timerID);
-    }, []);
+    setTime(now);
+  };
 
-    return (
-        <Fragment>
-            <HStack>
-                <Heading
-                    fontWeight={600}
-                    fontSize={{ base: "xl", sm: "2xl", md: "4xl" }}
-                    lineHeight={"110%"}
-                >
-                    <Text as={"span"} color={"green.400"}>
-                        {greeting}
-                        {userMeta &&
-                            `, ${userMeta.name} (${userMeta.userName})`}
-                    </Text>
-                </Heading>
+  useEffect(() => {
+    greet();
+    const timerID = setInterval(greet, 1000);
+    return () => clearInterval(timerID);
+  }, []);
 
-                {icon}
-            </HStack>
-            <Text color={"gray.500"}>
-                {time.toLocaleString("en-US", timeOptions)}
-            </Text>
-        </Fragment>
-    );
+  return (
+    <Fragment>
+      <HStack>
+        <Heading
+          fontWeight={600}
+          fontSize={{ base: "xl", sm: "2xl", md: "4xl" }}
+          lineHeight={"110%"}
+        >
+          <Text as={"span"} color={"green.400"}>
+            {greeting}
+            {userMeta && `, ${userMeta.name} (${userMeta.userName})`}
+          </Text>
+        </Heading>
+
+        {icon}
+      </HStack>
+      <Text color={"gray.500"}>
+        {time.toLocaleString("en-US", timeOptions)}
+      </Text>
+    </Fragment>
+  );
 };
 
 export default Greeting;
